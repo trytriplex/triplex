0dc62b44c8cdde5a1b76d47c151c1456ebbb7446
DOUGES <itsdouges@users.noreply.github.com>
2023-01-26 14:39:29 +1100

    feat: add infinite grid

--- apps/faze/src/box.tsx
+++ apps/faze/src/box.tsx
@@ -1,7 +1,13 @@
 export default function Box() {
   return (
-    <mesh>
-      <boxGeometry args={[1, 1, 1]} />
-    </mesh>
+    <>
+      <mesh position={[-2.580469294880466, 0, -2.475584516365517]}>
+        <boxGeometry args={[1, 1, 1]} />
+      </mesh>
+
+      <mesh>
+        <boxGeometry args={[1, 1, 1]} />
+      </mesh>
+    </>
   );
 }
