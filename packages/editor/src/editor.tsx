683f4338665b128db5eb6ca822c1b55e12a0a096
DOUGES <itsdouges@users.noreply.github.com>
2023-01-26 22:59:16 +1100

    feat: wrap in grid
    adeira-source-id: 7dc51783c64911ee8536c1806ce8fa95521b816d

--- packages/editor/src/editor.tsx
+++ packages/editor/src/editor.tsx
@@ -1,21 +1,27 @@
 import { useSearchParams } from "react-router-dom";
-import { View } from "@adobe/react-spectrum";
+import { View, Grid } from "@adobe/react-spectrum";
 
 export function Editor() {
   const [searchParams] = useSearchParams();
   const path = searchParams.get("path") || "";
 
   return (
-    <View backgroundColor="static-black" position="relative" height="100vh">
-      <iframe
-        style={{
-          border: 0,
-          position: "absolute",
-          width: "100%",
-          height: "100%",
-        }}
-        src={`/scene.html?path=${path}`}
-      />
-    </View>
+    <Grid areas={["content"]} columns={["auto"]} rows={["auto"]} height="100vh">
+      <View
+        gridArea="content"
+        backgroundColor="static-black"
+        position="relative"
+      >
+        <iframe
+          style={{
+            border: 0,
+            position: "absolute",
+            width: "100%",
+            height: "100%",
+          }}
+          src={`/scene.html?path=${path}`}
+        />
+      </View>
+    </Grid>
   );
 }
