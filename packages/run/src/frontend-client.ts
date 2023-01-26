281500e7b03f57833c781460d438ac8dacfc88a7
DOUGES <itsdouges@users.noreply.github.com>
2023-01-26 12:00:54 +1100

    chore: add examples
    adeira-source-id: 5eeee3dc241e6924fcc6ed5b30b25b37a1f0f4c2

--- packages/run/src/frontend-client.ts
+++
@@ -1,1 +0,0 @@
-import "@triplex/editor";
--- packages/run/src/frontend-server.ts
+++ packages/run/src/frontend-server.ts
@@ -1,21 +1,10 @@
-import { dirname, join } from "path";
-import { mkdir, symlink } from "fs/promises";
-import { fileURLToPath } from "url";
 import { createServer as createViteServer } from "vite";
 import react from "@vitejs/plugin-react";
 import glsl from "vite-plugin-glsl";
 
 const root = process.cwd();
-const triplexTmpPath = join(root, "node_modules", ".triplex");
-const runPkgPath = join(dirname(fileURLToPath(import.meta.url)), "..");
 
 export async function createServer(config: { publicDir?: string }) {
-  try {
-    await symlink(runPkgPath, triplexTmpPath);
-  } catch (e) {
-    // Dir already exists
-  }
-
   const frontendServer = await createViteServer({
     configFile: false,
     define: {
@@ -23,9 +12,6 @@ export async function createServer(config: { publicDir?: string }) {
     },
     plugins: [(react as any)(), glsl()],
     publicDir: config.publicDir,
-    resolve: {
-      preserveSymlinks: true,
-    },
     root,
   });
 
