dccad7da3fe11801974ce2acd77f4cce06a26b3a
DOUGES <itsdouges@users.noreply.github.com>
2023-01-26 13:29:47 +1100

    fix: get app running
    adeira-source-id: 8f4de32e7961cbd4daa59b318c8971cb3fa12552

--- packages/run/src/frontend-server.ts
+++ packages/run/src/frontend-server.ts
@@ -1,10 +1,18 @@
 import { createServer as createViteServer } from "vite";
 import react from "@vitejs/plugin-react";
 import { join } from "path";
+import { mkdir } from "fs/promises";
 
 const root = join(process.cwd(), "node_modules", "@triplex/run");
+const tempDir = join(process.cwd(), "node_modules", ".triplex");
 
 export async function createServer(config: { publicDir?: string }) {
+  try {
+    await mkdir(tempDir);
+  } catch (e) {
+    // Already created
+  }
+
   const glsl = (await import("vite-plugin-glsl")).default;
 
   const frontendServer = await createViteServer({
@@ -15,6 +23,11 @@ export async function createServer(config: { publicDir?: string }) {
     plugins: [(react as any)(), glsl()],
     publicDir: config.publicDir,
     root,
+    resolve: {
+      alias: {
+        "@@": tempDir,
+      },
+    },
   });
 
   return frontendServer;
