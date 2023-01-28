bf075f27717bb0c0baa7378df36f35ff00dfca6b
DOUGES <itsdouges@users.noreply.github.com>
2023-01-28 20:30:01 +1100

    fix: use ssr mode to remove need for html files

--- packages/run/src/frontend-server.ts
+++ packages/run/src/frontend-server.ts
@@ -1,11 +1,12 @@
 import { createServer as createViteServer, Plugin } from "vite";
 import react from "@vitejs/plugin-react";
 import { join } from "path";
-import { mkdir } from "fs/promises";
+import express from "express";
 import { watch } from "chokidar";
+import { createHTML } from "./templates";
 
+const root = process.cwd();
 const tempFolderName = join("node_modules", ".triplex");
-const root = join(process.cwd(), "node_modules", "@triplex/run");
 const tempDir = join(process.cwd(), tempFolderName);
 
 function forceGlobsHmr(): Plugin[] {
@@ -33,20 +34,17 @@ function forceGlobsHmr(): Plugin[] {
 }
 
 export async function createServer(config: { publicDir?: string }) {
-  try {
-    await mkdir(tempDir);
-  } catch (e) {
-    // Already created
-  }
-
+  const app = express();
   const glsl = (await import("vite-plugin-glsl")).default;
 
-  const frontendServer = await createViteServer({
+  const vite = await createViteServer({
     configFile: false,
     plugins: [(react as any)(), glsl(), forceGlobsHmr()],
     publicDir: config.publicDir,
     root,
+    appType: "custom",
     server: {
+      middlewareMode: true,
       watch: {
         ignored: [join("!**", tempFolderName, "**")],
       },
@@ -58,5 +56,43 @@ export async function createServer(config: { publicDir?: string }) {
     },
   });
 
-  return frontendServer;
+  app.use(vite.middlewares);
+
+  app.get("/scene.html", async (req, res, next) => {
+    const url = req.originalUrl;
+
+    try {
+      const template = createHTML(
+        "r3f_scene",
+        "/node_modules/@triplex/run/scene"
+      );
+      const html = await vite.transformIndexHtml(url, template);
+
+      res.status(200).set({ "Content-Type": "text/html" }).end(html);
+    } catch (e) {
+      vite.ssrFixStacktrace(e as Error);
+      next(e);
+    }
+  });
+
+  app.get("*", async (req, res, next) => {
+    const url = req.originalUrl;
+
+    try {
+      const template = createHTML(
+        "TRIPLEX",
+        "/node_modules/@triplex/run/editor"
+      );
+      const html = await vite.transformIndexHtml(url, template);
+
+      res.status(200).set({ "Content-Type": "text/html" }).end(html);
+    } catch (e) {
+      vite.ssrFixStacktrace(e as Error);
+      next(e);
+    }
+  });
+
+  return {
+    listen: (port: number) => app.listen(port),
+  };
 }
