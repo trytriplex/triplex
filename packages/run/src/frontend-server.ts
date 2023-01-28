a9546008af057d4712ee7ebd89263513013f2078
DOUGES <itsdouges@users.noreply.github.com>
2023-01-28 14:17:41 +1100

    fix: fixes globs not being picked up by hmr

--- packages/run/src/frontend-server.ts
+++ packages/run/src/frontend-server.ts
@@ -1,11 +1,36 @@
-import { createServer as createViteServer } from "vite";
+import { createServer as createViteServer, Plugin } from "vite";
 import react from "@vitejs/plugin-react";
 import { join } from "path";
 import { mkdir } from "fs/promises";
+import { watch } from "chokidar";
 
 const tempFolderName = join("node_modules", ".triplex");
 const root = join(process.cwd(), "node_modules", "@triplex/run");
 const tempDir = join(process.cwd(), tempFolderName);
+
+function forceGlobsHmr(): Plugin[] {
+  const triplexGlobModule = "triplex-scene-glob";
+
+  return [
+    {
+      name: "triplex/watch-globs",
+      configureServer(server) {
+        function reloadGlobModule() {
+          server.moduleGraph.fileToModulesMap.forEach((mods) => {
+            mods.forEach((mod) => {
+              if (mod.id?.includes(triplexGlobModule)) {
+                server.reloadModule(mod);
+              }
+            });
+          });
+        }
+
+        const watcher = watch(tempDir);
+        watcher.on("add", reloadGlobModule);
+      },
+    },
+  ];
+}
 
 export async function createServer(config: { publicDir?: string }) {
   try {
@@ -18,7 +43,7 @@ export async function createServer(config: { publicDir?: string }) {
 
   const frontendServer = await createViteServer({
     configFile: false,
-    plugins: [(react as any)(), glsl()],
+    plugins: [(react as any)(), glsl(), forceGlobsHmr()],
     publicDir: config.publicDir,
     root,
     server: {
---
+++ packages/scene/src/triplex-scene-glob.tsx
@@ -0,0 +1,1 @@
+export const sceneModules = import.meta.glob("@@/**/*.tsx");
