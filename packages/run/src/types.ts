71ffcba292ffe26737ea648fd08fe92529ad3cbc
Michael Dougall <6801309+itsdouges@users.noreply.github.com>
2023-04-22 14:28:04 +1000

    JSON schema (#160)
    Co-authored-by: DOUGES <itsdouges@users.noreply.github.com>

--- packages/run/src/types.ts
+++ packages/run/src/types.ts
@@ -1,14 +1,20 @@
 export interface TRIPLEXConfig {
   /**
-   * Relative filepath globs used to mark files that are able to be added to scenes.
+   * A glob array of relative paths used to tell Triplex what files should be
+   * available to add. You can define multiple roots.
    */
   components?: string[];
   /**
-   * Relative filepath globs used to mark files that can be opened by the editor.
+   * A glob array of relative paths used to tell Triplex what files it can open.
+   * You can define multiple roots. These found files will be displayed in the
+   * `File` > `Open` menu.
    */
   files: string[];
   /**
-   * Relative path to a folder which contains static assets such as gltf files.
+   * Used to expose files through the local dev server, such as images, videos,
+   * and GLTF files. For example if the public directory includes a
+   * `public/image.png` image it will be made available at
+   * `http://localhost:3333/image.png`.
    */
   publicDir?: string;
 }
