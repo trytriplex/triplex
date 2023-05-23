e3959ba2df6bf6606d9cf8dde6e26a8a875b24d1
Michael Dougall <6801309+itsdouges@users.noreply.github.com>
2023-05-23 12:41:27 +1000

    Adeira shipit (#219)
    Co-authored-by: DOUGES <itsdouges@users.noreply.github.com>

---
+++ .shipit/triplex.js
@@ -0,0 +1,32 @@
+module.exports = {
+  getStaticConfig() {
+    return {
+      repository: "git@github.com:try-triplex/triplex.git",
+    };
+  },
+  getPathMappings() {
+    return new Map([
+      ["apps", "apps"],
+      ["examples", "examples"],
+      ["packages", "packages"],
+    ]);
+  },
+  getStrippedFiles() {
+    return new Set([
+      /^\.github\/workflows\/release-electron\.yml/,
+      /^.shipit/,
+      /^apps\/electron/,
+      /^packages\/bridge/,
+      /^packages\/client/,
+      /^packages\/server/,
+      /^packages\/ws-client/,
+      /^scripts/,
+    ]);
+  },
+  getBranchConfig() {
+    return {
+      source: "main",
+      destination: "test",
+    };
+  },
+};
