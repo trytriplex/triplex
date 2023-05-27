fc7123cac77318de58b472746610133b507c391c
Michael Dougall <6801309+itsdouges@users.noreply.github.com>
2023-05-27 19:23:24 +1000

    Fix download layout (#249)
    Co-authored-by: DOUGES <itsdouges@users.noreply.github.com>

--- apps/docs/components/landing.tsx
+++ apps/docs/components/landing.tsx
@@ -8,13 +8,13 @@ export function Landing() {
           Visually Edit React Three Fiber Components
         </h1>
 
-        <span className="text-xl text-neutral-300">
-          Have changes saved straight back to your source code.
+        <span className="text-center text-xl text-neutral-300">
+          Save your changes straight back to source code.
         </span>
 
         <Link
           href="/docs/overview"
-          className="rounded-full border-4 border-blue-400 px-10 py-4 text-xl font-bold text-blue-400 shadow-2xl shadow-blue-400/20 hover:bg-blue-400 hover:text-neutral-900 md:ml-auto lg:text-2xl xl:mr-28"
+          className="rounded-full border-[3px] border-blue-400 px-10 py-4 text-xl font-semibold text-blue-400 shadow-2xl shadow-blue-400/20 hover:bg-blue-400 hover:text-neutral-900 md:ml-auto lg:text-2xl xl:mr-28"
         >
           Get Started →
         </Link>
