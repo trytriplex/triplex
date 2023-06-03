a69406177b440b5d7abe2ff65db412546f22ea6a
Michael Dougall <6801309+itsdouges@users.noreply.github.com>
2023-06-03 22:15:37 +1000

    Remove log hacks (#269)

--- apps/electron/src/util/log.ts
+++ apps/electron/src/util/log.ts
@@ -1,19 +1,10 @@
 import log from "electron-log";
-// @ts-expect-error - We're dipping into internals to be able to get the path location
-// Hopefully it will be officially exposed and we don't need to do this soon.
-// See: https://github.com/megahertz/electron-log/discussions/333#discussioncomment-5835657
-import variables from "electron-log/src/main/transports/file/variables";
 
 log.initialize({ preload: true, spyRendererConsole: true });
 Object.assign(console, log.functions);
 
-const pathVariables = variables.getPathVariables(process.platform);
-
 export const getLogPath = (): string => {
-  return log.transports.file.resolvePathFn({
-    ...pathVariables,
-    fileName: log.transports.file.fileName,
-  });
+  return log.transports.file.getFile().path;
 };
 
 export function logger(scope: string) {
