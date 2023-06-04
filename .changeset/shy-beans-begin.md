120f9eff30ec98b47ee2afd3074102b52b884a58
Michael Dougall <6801309+itsdouges@users.noreply.github.com>
2023-06-04 18:39:00 +1000

    Fix how array input handles optional undefined and empty string values (#271)

---
+++ .changeset/shy-beans-begin.md
@@ -0,0 +1,6 @@
+---
+"@triplex/editor": patch
+---
+
+When updating array inputs undefined optional values are now ignored and
+discarded.
--- packages/editor/src/ui/__tests__/array-input.test.tsx
+++ packages/editor/src/ui/__tests__/array-input.test.tsx
@@ -141,4 +141,59 @@ describe("array input", () => {
     expect(change).toHaveBeenCalledWith([undefined, 33]);
     expect(confirm).toHaveBeenCalledWith([undefined, 33]);
   });
+
+  it("should remove optional undefined values from right to left until hitting a defined value", () => {
+    const change = vi.fn();
+    const confirm = vi.fn();
+    const { getByTestId } = render(
+      <ArrayInput
+        column={0}
+        line={0}
+        values={[
+          { type: "number", value: 66, required: true },
+          { type: "number", value: 77, required: true },
+          { type: "string", value: undefined },
+        ]}
+        name="array"
+        path="/box.tsx"
+        onChange={change}
+        onConfirm={confirm}
+      />
+    );
+    const first = getByTestId("number-66");
+
+    fireEvent.change(first, { target: { value: 101 } });
+    fireEvent.blur(first, { target: { value: 101 } });
+
+    expect(change).toHaveBeenCalledWith([101, 77]);
+    expect(confirm).toHaveBeenCalledWith([101, 77]);
+  });
+
+  it("should remove optional undefined values from right to left until hitting a defined value", () => {
+    const change = vi.fn();
+    const confirm = vi.fn();
+    const { getByTestId } = render(
+      <ArrayInput
+        column={0}
+        line={0}
+        values={[
+          { type: "number", value: 888, required: true },
+          { type: "number", value: undefined },
+          { type: "number", value: 999, required: true },
+          { type: "string", value: undefined },
+        ]}
+        name="array"
+        path="/box.tsx"
+        onChange={change}
+        onConfirm={confirm}
+      />
+    );
+    const first = getByTestId("number-888");
+
+    fireEvent.change(first, { target: { value: 666 } });
+    fireEvent.blur(first, { target: { value: 666 } });
+
+    expect(change).toHaveBeenCalledWith([666, undefined, 999]);
+    expect(confirm).toHaveBeenCalledWith([666, undefined, 999]);
+  });
 });
