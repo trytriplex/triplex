7b3d9f33a8c20f71188eaf3e0c3bf9c5ed51d807
Michael Dougall <6801309+itsdouges@users.noreply.github.com>
2023-06-10 14:16:12 +1000

    Display unsupported platform for download button when viewed on unsupported platforms
    adeira-source-id: 42daaeaa5fed91cbdbd1d188c44f6d8f12e22035

---
+++ apps/docs/util/cn.ts
@@ -0,0 +1,36 @@
+type ClassNames = string | false | undefined | ClassNames[];
+
+function collectDecls(
+  classNames: ClassNames[],
+  outDecls: Record<string, string>
+): void {
+  for (let i = 0; i < classNames.length; i++) {
+    const className = classNames[i];
+
+    if (Array.isArray(className)) {
+      collectDecls(className, outDecls);
+    } else if (className) {
+      const parts = className.split(" ");
+
+      for (let x = 0; x < parts.length; x++) {
+        const part = parts[x];
+        outDecls[part] = part;
+      }
+    }
+  }
+}
+
+export function cn(classNames: ClassNames[]): string | undefined {
+  const decls: Record<string, string> = {};
+
+  collectDecls(classNames, decls);
+
+  const str = [];
+
+  for (const key in decls) {
+    const value = decls[key];
+    str.push(value);
+  }
+
+  return str.join(" ");
+}
