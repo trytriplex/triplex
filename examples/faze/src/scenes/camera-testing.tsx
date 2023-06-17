8026cde3062dc854c96269ef74fa99f470f4b6b6
Michael Dougall <6801309+itsdouges@users.noreply.github.com>
2023-06-17 22:05:37 +1000

    Update faze camera example
    adeira-source-id: 27dfdbd0f2fe89259dcb6c24f43804391daf4ed4

--- examples/faze/src/scenes/camera-testing.tsx
+++ examples/faze/src/scenes/camera-testing.tsx
@@ -15,9 +15,27 @@ export function Scene() {
         offset={[-4.25, 5, 5.5]}
       />
 
-      <PlayerEntity position={[-0.9604949128687132, 0, 0]} />
+      <PlayerEntity position={[0, 0, 0]} />
 
-      <PerspectiveCamera position={[0, 0, 0.7469631272109094]} />
+      <PerspectiveCamera
+        position={[-0.14164693646901694, 1.7321220378277902, 2.068985775667036]}
+        rotation={[-0.24980476564436316, 0, 0]}
+      />
+
+      <mesh
+        position={[-0.06142748306858481, -0.10498960256857615, 0]}
+        receiveShadow={true}
+      >
+        <boxGeometry args={[10, 0.2, 5]} />
+        <meshStandardMaterial />
+      </mesh>
+      <pointLight
+        position={[-1.997110931458752, 5.560439855681761, -0.9603405372967642]}
+        castShadow={true}
+        color={"#a8cfd7"}
+      />
+
+      <ambientLight intensity={0.2} />
     </>
   );
 }
