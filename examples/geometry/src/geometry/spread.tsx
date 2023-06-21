881d3f07d504cab092be6f0b0f7e1a01dd796079
Michael Dougall <6801309+itsdouges@users.noreply.github.com>
2023-06-21 17:52:17 +1000

    Fix spread props usage for transform props
    adeira-source-id: ad66cc802fe4a5e78f2645097c57dfe4fb1f48dd

---
+++ examples/geometry/src/geometry/spread.tsx
@@ -0,0 +1,18 @@
+import { Vector3Tuple } from "three";
+
+export function ThisSpreadProps(props: { position: Vector3Tuple }) {
+  return (
+    <group {...props}>
+      <mesh>
+        <boxGeometry />
+        <meshStandardMaterial color="red" />
+      </mesh>
+      <mesh>
+        <cylinderGeometry
+          args={[undefined, undefined, 2, undefined, undefined, false]}
+        />
+        <meshStandardMaterial color="green" />
+      </mesh>
+    </group>
+  );
+}
