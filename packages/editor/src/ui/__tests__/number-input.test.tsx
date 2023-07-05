270165e74e038b3a82ee6896a05fe814579ead00
Michael Dougall <6801309+itsdouges@users.noreply.github.com>
2023-07-05 13:28:47 +1000

    Fix number input edge cases and add test coverage
    adeira-source-id: b3d2fa929e2387dcf6641d817954688a5e590f2e

--- packages/editor/src/ui/__tests__/number-input.test.tsx
+++ packages/editor/src/ui/__tests__/number-input.test.tsx
@@ -5,11 +5,20 @@
  * file in the root directory of this source tree.
  */
 // @vitest-environment jsdom
-import { describe, expect, it, vi } from "vitest";
-import { render, fireEvent } from "@testing-library/react";
+import { afterEach, describe, expect, it, vi } from "vitest";
+import {
+  render,
+  fireEvent,
+  cleanup,
+  waitFor,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
 import { NumberInput } from "../number-input";
+import { PropTagContext } from "../prop-input";
 
 describe("number input", () => {
+  afterEach(cleanup);
+
   it("should transform the set value", () => {
     const change = vi.fn();
     const confirm = vi.fn();
@@ -52,5 +61,350 @@ describe("number input", () => {
     fireEvent.blur(element, { target: { value: 19 } });
 
     expect(change).toHaveBeenCalledWith(9);
+  });
+
+  it("should decrement input", () => {
+    const change = vi.fn();
+    const confirm = vi.fn();
+    const { getByLabelText } = render(
+      <NumberInput
+        name="x"
+        defaultValue={10}
+        onChange={change}
+        onConfirm={confirm}
+      />
+    );
+    const element = getByLabelText("Decrease by 0.02");
+
+    fireEvent.click(element);
+    fireEvent.blur(element);
+
+    expect(change).toHaveBeenCalledWith(9.98);
+    expect(confirm).toHaveBeenCalledWith(9.98);
+  });
+
+  it("should increment input", () => {
+    const change = vi.fn();
+    const confirm = vi.fn();
+    const { getByLabelText } = render(
+      <NumberInput
+        name="x"
+        defaultValue={10}
+        onChange={change}
+        onConfirm={confirm}
+      />
+    );
+    const element = getByLabelText("Increase by 0.02");
+
+    fireEvent.click(element);
+    fireEvent.blur(element);
+
+    expect(change).toHaveBeenCalledWith(10.02);
+    expect(confirm).toHaveBeenCalledWith(10.02);
+  });
+
+  it("should clear value", () => {
+    const change = vi.fn();
+    const confirm = vi.fn();
+    const { getByLabelText } = render(
+      <NumberInput
+        name="x"
+        defaultValue={10}
+        onChange={change}
+        onConfirm={confirm}
+      />
+    );
+    const element = getByLabelText("Clear value");
+
+    fireEvent.click(element);
+    fireEvent.blur(element);
+
+    expect(change).toHaveBeenCalledWith(undefined);
+    expect(confirm).toHaveBeenCalledWith(undefined);
+  });
+
+  it("should focus the input when not initiating a drag", async () => {
+    const { getByTestId } = render(
+      <NumberInput
+        name="x"
+        defaultValue={10}
+        onChange={() => {}}
+        onConfirm={() => {}}
+      />
+    );
+    const element = getByTestId("number-10");
+
+    fireEvent.mouseDown(element);
+    await waitFor(() => getByTestId("pointer-lock"));
+    fireEvent.mouseUp(element);
+
+    expect(document.activeElement).toBe(element);
+  });
+
+  it("should not focus the input when initiating a drag", async () => {
+    const { getByTestId } = render(
+      <NumberInput
+        name="x"
+        defaultValue={10}
+        onChange={() => {}}
+        onConfirm={() => {}}
+      />
+    );
+    const element = getByTestId("number-10");
+
+    fireEvent.mouseDown(element);
+    await waitFor(() => getByTestId("pointer-lock"));
+    fireEvent.mouseMove(element);
+    fireEvent.mouseUp(element);
+
+    expect(document.activeElement).not.toBe(element);
+  });
+
+  it("should blur away from an iframe on mouse down", () => {
+    const { getByTestId } = render(
+      <>
+        <iframe data-testid="iframe" />
+        <NumberInput
+          name="x"
+          defaultValue={10}
+          onChange={() => {}}
+          onConfirm={() => {}}
+        />
+      </>
+    );
+    const iframe = getByTestId("iframe");
+    iframe.focus();
+    const element = getByTestId("number-10");
+
+    fireEvent.mouseDown(element);
+
+    expect(document.activeElement).not.toBe(iframe);
+  });
+
+  it("should do nothing when there has been no movement in a drag", async () => {
+    const change = vi.fn();
+    const confirm = vi.fn();
+    const { getByTestId } = render(
+      <NumberInput
+        name="x"
+        defaultValue={10}
+        onChange={change}
+        onConfirm={confirm}
+      />
+    );
+    const element = getByTestId("number-10");
+
+    fireEvent.mouseDown(element);
+    await waitFor(() => getByTestId("pointer-lock"));
+    fireEvent.mouseMove(element, { clientX: 0 });
+
+    expect(change).toHaveBeenCalledWith(10);
+  });
+
+  it("should increment when dragging movement is positive", async () => {
+    const change = vi.fn();
+    const confirm = vi.fn();
+    const { getByTestId } = render(
+      <NumberInput
+        name="x"
+        defaultValue={10}
+        onChange={change}
+        onConfirm={confirm}
+      />
+    );
+    const element = getByTestId("number-10");
+    fireEvent.mouseDown(element);
+    await waitFor(() => getByTestId("pointer-lock"));
+
+    fireEvent.mouseMove(element, { clientX: 1 });
+
+    expect(change).toHaveBeenCalledWith(10.02);
+  });
+
+  it("should multiply the step during a drag when pressing ctrl", async () => {
+    const change = vi.fn();
+    const confirm = vi.fn();
+    const { getByTestId } = render(
+      <NumberInput
+        name="x"
+        defaultValue={10.2}
+        onChange={change}
+        onConfirm={confirm}
+      />
+    );
+    const element = getByTestId("number-10.2");
+    fireEvent.mouseDown(element);
+    await waitFor(() => getByTestId("pointer-lock"));
+    await fireEvent.keyDown(document, { ctrlKey: true });
+
+    fireEvent.mouseMove(element, { clientX: 1 });
+
+    // TODO: This should be capped to multiples of 0.5
+    // E.g. Round up or down. So this should round down to 10.5.
+    expect(change).toHaveBeenCalledWith(10.7);
+  });
+
+  it("should reduce the step during a drag when pressing shift", async () => {
+    const change = vi.fn();
+    const confirm = vi.fn();
+    const { getByTestId } = render(
+      <NumberInput
+        name="x"
+        defaultValue={10.2}
+        onChange={change}
+        onConfirm={confirm}
+      />
+    );
+    const element = getByTestId("number-10.2");
+    fireEvent.mouseDown(element);
+    await waitFor(() => getByTestId("pointer-lock"));
+    await fireEvent.keyDown(document, { shiftKey: true });
+
+    fireEvent.mouseMove(element, { clientX: 1 });
+
+    expect(change).toHaveBeenCalledWith(10.202);
+  });
+
+  it("should decrement when dragging movement is negative", async () => {
+    const change = vi.fn();
+    const confirm = vi.fn();
+    const { getByTestId } = render(
+      <NumberInput
+        name="x"
+        defaultValue={10}
+        onChange={change}
+        onConfirm={confirm}
+      />
+    );
+    const element = getByTestId("number-10");
+    fireEvent.mouseDown(element);
+    await waitFor(() => getByTestId("pointer-lock"));
+
+    fireEvent.mouseMove(element, { clientX: -1 });
+
+    expect(change).toHaveBeenCalledWith(9.98);
+  });
+
+  it("should multiply the change when movement is over multiple pixels", async () => {
+    const change = vi.fn();
+    const confirm = vi.fn();
+    const { getByTestId } = render(
+      <NumberInput
+        name="x"
+        defaultValue={10}
+        onChange={change}
+        onConfirm={confirm}
+      />
+    );
+    const element = getByTestId("number-10");
+    fireEvent.mouseDown(element);
+    await waitFor(() => getByTestId("pointer-lock"));
+
+    fireEvent.mouseMove(element, { clientX: 100 });
+
+    expect(change).toHaveBeenCalledWith(12);
+  });
+
+  it("should callback with confirm when completing a drag", async () => {
+    const change = vi.fn();
+    const confirm = vi.fn();
+    const { getByTestId } = render(
+      <NumberInput
+        name="x"
+        defaultValue={10}
+        onChange={change}
+        onConfirm={confirm}
+      />
+    );
+    const element = getByTestId("number-10");
+    fireEvent.mouseDown(element);
+    await waitFor(() => getByTestId("pointer-lock"));
+    fireEvent.mouseMove(element, { clientX: 100 });
+
+    fireEvent.mouseUp(element);
+    await waitForElementToBeRemoved(() => getByTestId("pointer-lock"));
+
+    expect(confirm).toHaveBeenCalledWith(12);
+  });
+
+  it("should focus the input when clearing via the clear button", () => {
+    const { getByLabelText, getByTestId } = render(
+      <NumberInput
+        name="x"
+        defaultValue={10}
+        onChange={() => {}}
+        onConfirm={() => {}}
+      />
+    );
+    const element = getByLabelText("Clear value");
+    const inputElement = getByTestId("number-10");
+
+    fireEvent.click(element);
+
+    expect(document.activeElement).toBe(inputElement);
+  });
+
+  it("should not callback if value is outside max range", () => {
+    const change = vi.fn();
+    const confirm = vi.fn();
+    const { getByTestId } = render(
+      <PropTagContext.Provider value={{ max: 100 }}>
+        <NumberInput
+          name="x"
+          defaultValue={10}
+          onChange={change}
+          onConfirm={confirm}
+        />
+      </PropTagContext.Provider>
+    );
+    const element = getByTestId("number-10");
+
+    fireEvent.change(element, { target: { value: "1000" } });
+    fireEvent.blur(element);
+
+    expect(change).not.toHaveBeenCalled();
+    expect(confirm).not.toHaveBeenCalled();
+  });
+
+  it("should not callback if value is outside max range", () => {
+    const change = vi.fn();
+    const confirm = vi.fn();
+    const { getByTestId } = render(
+      <PropTagContext.Provider value={{ min: 0 }}>
+        <NumberInput
+          name="x"
+          defaultValue={10}
+          onChange={change}
+          onConfirm={confirm}
+        />
+      </PropTagContext.Provider>
+    );
+    const element = getByTestId("number-10");
+
+    fireEvent.change(element, { target: { value: "-1000" } });
+    fireEvent.blur(element);
+
+    expect(change).not.toHaveBeenCalled();
+    expect(confirm).not.toHaveBeenCalled();
+  });
+
+  it("should not callback if nothing has changed", () => {
+    const change = vi.fn();
+    const confirm = vi.fn();
+    const { getByTestId } = render(
+      <NumberInput
+        name="x"
+        defaultValue={10}
+        onChange={change}
+        onConfirm={confirm}
+      />
+    );
+    const element = getByTestId("number-10");
+
+    fireEvent.change(element, { target: { value: "10" } });
+    fireEvent.blur(element);
+
+    expect(change).not.toHaveBeenCalled();
+    expect(confirm).not.toHaveBeenCalled();
   });
 });
