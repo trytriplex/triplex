f97f6a1d51cd5459697fd354aa6940fb7880c3c1
Michael Dougall <6801309+itsdouges@users.noreply.github.com>
2023-09-30 21:55:49 +1000

    Breakfast scene lighting
    adeira-source-id: eb9aac69bdfa3c6ea19f82682c30003e31225de3

--- examples/geometry/src/breakfast.tsx
+++ examples/geometry/src/breakfast.tsx
@@ -25,7 +25,7 @@ export function Frypan({
   position?: Vector3Tuple;
   rotation?: Vector3Tuple;
 }) {
-  const { nodes } = useGLTF("/assets/breakfast.glb");
+  const { nodes, materials } = useGLTF("/assets/breakfast.glb");
 
   return (
     <RigidBody
@@ -35,7 +35,9 @@ export function Frypan({
       mass={1}
       density={100}
     >
-      <Clone object={nodes.frypan} />
+      <Clone castShadow receiveShadow object={nodes.frypan}>
+        <meshStandardMaterial map={materials["lambert6SG"].map} />
+      </Clone>
     </RigidBody>
   );
 }
@@ -49,21 +51,25 @@ export function Bacon({
   position?: Vector3Tuple;
   rotation?: Vector3Tuple;
 }) {
-  const { nodes } = useGLTF("/assets/breakfast.glb");
+  const { nodes, materials } = useGLTF("/assets/breakfast.glb");
 
   return (
     <RigidBody rotation={rotation} position={position} colliders={"trimesh"}>
-      <Clone object={nodes[variant]} />
+      <Clone castShadow receiveShadow object={nodes[variant]}>
+        <meshStandardMaterial map={materials["lambert6SG"].map} />
+      </Clone>
     </RigidBody>
   );
 }
 
 export function CuttingBoard({ position }: { position?: Vector3Tuple }) {
-  const { nodes } = useGLTF("/assets/breakfast.glb");
+  const { nodes, materials } = useGLTF("/assets/breakfast.glb");
 
   return (
     <CuboidCollider position={position} args={[1.92, 0.14, 1.32]}>
-      <Clone object={nodes.cutting_board} />
+      <Clone castShadow receiveShadow object={nodes.cutting_board}>
+        <meshStandardMaterial map={materials["lambert7SG"].map} />
+      </Clone>
     </CuboidCollider>
   );
 }
@@ -75,11 +81,13 @@ export function Cup({
   position?: Vector3Tuple;
   rotation?: Vector3Tuple;
 }) {
-  const { nodes } = useGLTF("/assets/breakfast.glb");
+  const { nodes, materials } = useGLTF("/assets/breakfast.glb");
 
   return (
     <RigidBody position={position} rotation={rotation} colliders={"cuboid"}>
-      <Clone object={nodes.coffee_cup} />
+      <Clone castShadow receiveShadow object={nodes.coffee_cup}>
+        <meshStandardMaterial map={materials["lambert7SG"].map} />
+      </Clone>
     </RigidBody>
   );
 }
@@ -91,11 +99,13 @@ export function Egg({
   position?: Vector3Tuple;
   rotation?: Vector3Tuple;
 }) {
-  const { nodes } = useGLTF("/assets/breakfast.glb");
+  const { nodes, materials } = useGLTF("/assets/breakfast.glb");
 
   return (
     <RigidBody rotation={rotation} position={position} colliders={"cuboid"}>
-      <Clone object={nodes.egg} />
+      <Clone castShadow receiveShadow object={nodes.egg}>
+        <meshStandardMaterial map={materials["lambert6SG"].map} />
+      </Clone>
     </RigidBody>
   );
 }
@@ -109,11 +119,13 @@ export function Sausage({
   rotation?: Vector3Tuple;
   variant?: "sausage_1" | "sausage_2";
 }) {
-  const { nodes } = useGLTF("/assets/breakfast.glb");
+  const { nodes, materials } = useGLTF("/assets/breakfast.glb");
 
   return (
     <RigidBody rotation={rotation} position={position} colliders={"trimesh"}>
-      <Clone object={nodes[variant]} />
+      <Clone castShadow receiveShadow object={nodes[variant]}>
+        <meshStandardMaterial map={materials["lambert6SG"].map} />
+      </Clone>
     </RigidBody>
   );
 }
@@ -127,12 +139,14 @@ export function Pancake({
   rotation?: Vector3Tuple;
   variant?: "pancake_1" | "pancake_2" | "pancake_3" | "pancake_4" | "pancake_5";
 }) {
-  const { nodes } = useGLTF("/assets/breakfast.glb");
+  const { nodes, materials } = useGLTF("/assets/breakfast.glb");
 
   return (
     <RigidBody rotation={rotation} position={position} colliders={false}>
       <CylinderCollider args={[0.07, 0.54]}>
-        <Clone object={nodes[variant]} />
+        <Clone castShadow receiveShadow object={nodes[variant]}>
+          <meshStandardMaterial map={materials["lambert7SG"].map} />
+        </Clone>
       </CylinderCollider>
     </RigidBody>
   );
@@ -145,11 +159,13 @@ export function Plate({
   position?: Vector3Tuple;
   rotation?: Vector3Tuple;
 }) {
-  const { nodes } = useGLTF("/assets/breakfast.glb");
+  const { nodes, materials } = useGLTF("/assets/breakfast.glb");
 
   return (
     <RigidBody rotation={rotation} position={position} colliders={"cuboid"}>
-      <Clone object={nodes.plate} />
+      <Clone castShadow receiveShadow object={nodes.plate}>
+        <meshStandardMaterial map={materials["lambert7SG"].map} />
+      </Clone>
     </RigidBody>
   );
 }
@@ -161,7 +177,7 @@ export function Butter({
   position?: Vector3Tuple;
   rotation?: Vector3Tuple;
 }) {
-  const { nodes } = useGLTF("/assets/breakfast.glb");
+  const { nodes, materials } = useGLTF("/assets/breakfast.glb");
 
   return (
     <RigidBody
@@ -170,7 +186,9 @@ export function Butter({
       colliders={"cuboid"}
       mass={0.1}
     >
-      <Clone object={nodes.butter} />
+      <Clone castShadow receiveShadow object={nodes.butter}>
+        <meshStandardMaterial map={materials["lambert7SG"].map} />
+      </Clone>
     </RigidBody>
   );
 }
@@ -184,7 +202,7 @@ export function Berry({
   rotation?: Vector3Tuple;
   variant?: "raspberry" | "blueberry";
 }) {
-  const { nodes } = useGLTF("/assets/breakfast.glb");
+  const { nodes, materials } = useGLTF("/assets/breakfast.glb");
 
   return (
     <RigidBody
@@ -193,137 +211,169 @@ export function Berry({
       colliders={"ball"}
       mass={0.1}
     >
-      <Clone object={nodes[variant]} />
+      <Clone castShadow receiveShadow object={nodes[variant]}>
+        <meshStandardMaterial map={materials["lambert6SG"].map} />
+      </Clone>
     </RigidBody>
   );
 }
 
 export function Breakfast() {
   return (
-    <group name="scene">
-      <group
-        name="frypan"
-        position={[0.1956764961142352, 0, 0.1757335858124467]}
-      >
-        <Sausage
-          position={[
-            0.5279772897177749, 0.348022386438613, 0.20743457175327318,
-          ]}
-          rotation={[
-            -3.141592653589793, -0.5507053355872314, -3.141592653589793,
-          ]}
-        />
-        <Sausage
-          variant={"sausage_2"}
-          rotation={[3.141592653589793, -0.6778986344070088, 3.141592653589793]}
-          position={[
-            0.25344678087265615, 0.5094750459061809, 0.2654945820241248,
-          ]}
-        />
-        <Egg
-          position={[1.1612973453430502, 0.76, -0.0944864069847154]}
-          rotation={[
-            -2.9763791974669767, -0.2784921613522725, 3.0241934151892575,
-          ]}
-        />
-        <Frypan
-          position={[1.35160562276846, 0.3272218215467831, -0.8301222069262155]}
-        />
-        <Bacon
-          position={[
-            0.6726499434639116, 0.4545375832703098, -0.6817538617658193,
-          ]}
-          variant={"bacon_1"}
-          rotation={[
-            3.047325691990559, -0.6102469703269914, 2.8757639096527483,
-          ]}
-        />
-      </group>
+    <>
+      <group name="scene">
+        <group
+          name="frypan"
+          position={[0.1956764961142352, 0, 0.1757335858124467]}
+        >
+          <Sausage
+            position={[
+              0.5279772897177749, 0.348022386438613, 0.20743457175327318,
+            ]}
+            rotation={[
+              -3.141592653589793, -0.5507053355872314, -3.141592653589793,
+            ]}
+          />
+          <Sausage
+            variant={"sausage_2"}
+            rotation={[
+              3.141592653589793, -0.6778986344070088, 3.141592653589793,
+            ]}
+            position={[
+              0.25344678087265615, 0.5094750459061809, 0.2654945820241248,
+            ]}
+          />
+          <Egg
+            position={[1.030514850082852, 0.76, -0.0944864069847154]}
+            rotation={[
+              -2.9763791974669767, -0.2784921613522725, 3.0241934151892575,
+            ]}
+          />
+          <Frypan
+            position={[
+              1.35160562276846, 0.3272218215467831, -0.8301222069262155,
+            ]}
+          />
+          <Bacon
+            position={[
+              0.6726499434639116, 0.4545375832703098, -0.6817538617658193,
+            ]}
+            variant={"bacon_1"}
+            rotation={[
+              3.047325691990559, -0.6102469703269914, 2.8757639096527483,
+            ]}
+          />
+        </group>
 
-      <CuttingBoard position={[0.2681269017277474, -0.13094897027373942, 0]} />
-      <Cup
-        position={[-1.0600194852633598, 0.5, -0.7380562035176215]}
-        rotation={[
-          -0.341743001454544, -0.9484979416396888, -0.28140817152508824,
-        ]}
+        <CuttingBoard
+          position={[0.2681269017277474, -0.13094897027373942, 0]}
+        />
+        <Cup
+          position={[
+            -1.0600194852633598, 0.9910881898387598, -0.7380562035176215,
+          ]}
+          rotation={[
+            -0.10864987535213397, -0.975917761242133, -0.10925378685024859,
+          ]}
+        />
+
+        <group
+          name="pancakes"
+          position={[0.000607940410560559, 0, 0.06351598035668421]}
+        >
+          <Berry
+            position={[
+              -1.0143258493350351, 1.6155004431233018, 0.18077933787745476,
+            ]}
+            variant={"raspberry"}
+          />
+          <Berry
+            position={[
+              -0.6439576461969428, 1.6155004431233018, 0.6192870191570334,
+            ]}
+            variant={"blueberry"}
+          />
+          <Berry
+            position={[
+              -0.561303621591706, 1.6155004431233018, 0.7292707609205257,
+            ]}
+            variant={"blueberry"}
+          />
+          <Berry
+            position={[
+              -1.0373627979094773, 1.6155004431233018, 0.4964366525983276,
+            ]}
+            variant={"raspberry"}
+            rotation={[
+              3.141592653589793, -1.4517589119805536, 3.141592653589793,
+            ]}
+          />
+          <Berry
+            position={[
+              -0.506898206793218, 1.6155004431233018, 0.5987784905561545,
+            ]}
+            variant={"blueberry"}
+          />
+          <Butter
+            position={[
+              -0.7957860280311837, 1.7246620573080738, 0.401354280745883,
+            ]}
+          />
+          <Pancake
+            position={[
+              -0.785455934059639, 1.4112836100133928, 0.44493200318372905,
+            ]}
+            variant={"pancake_1"}
+          />
+          <Pancake
+            position={[
+              -0.785455934059639, 1.1628608589922276, 0.44493200318372905,
+            ]}
+            variant={"pancake_2"}
+          />
+          <Pancake
+            position={[
+              -0.785455934059639, 0.8879944622230104, 0.44493200318372905,
+            ]}
+            variant={"pancake_3"}
+          />
+          <Pancake
+            position={[
+              -0.785455934059639, 0.6296357686801064, 0.44493200318372905,
+            ]}
+            variant={"pancake_4"}
+          />
+          <Pancake
+            position={[
+              -0.785455934059639, 0.3594528799751239, 0.44493200318372905,
+            ]}
+            variant={"pancake_5"}
+          />
+          <Plate
+            position={[
+              -0.8403565747507878, 0.11699105745133492, 0.47439501162598885,
+            ]}
+          />
+        </group>
+      </group>
+      <pointLight
+        position={[-1.1340643535780428, 2.381443147806335, 1.6547310228630394]}
+        castShadow={true}
+        receiveShadow={false}
       />
-
-      <group
-        name="pancakes"
-        position={[0.000607940410560559, 0, 0.06351598035668421]}
-      >
-        <Berry
-          position={[
-            -1.0143258493350351, 1.6155004431233018, 0.18077933787745476,
-          ]}
-          variant={"raspberry"}
-        />
-        <Berry
-          position={[
-            -0.6439576461969428, 1.6155004431233018, 0.6192870191570334,
-          ]}
-          variant={"blueberry"}
-        />
-        <Berry
-          position={[
-            -0.561303621591706, 1.6155004431233018, 0.7292707609205257,
-          ]}
-          variant={"blueberry"}
-        />
-        <Berry
-          position={[
-            -1.0373627979094773, 1.6155004431233018, 0.4964366525983276,
-          ]}
-          variant={"raspberry"}
-          rotation={[3.141592653589793, -1.4517589119805536, 3.141592653589793]}
-        />
-        <Berry
-          position={[
-            -0.506898206793218, 1.6155004431233018, 0.5987784905561545,
-          ]}
-          variant={"blueberry"}
-        />
-        <Butter
-          position={[
-            -0.7957860280311837, 1.7246620573080738, 0.401354280745883,
-          ]}
-        />
-        <Pancake
-          position={[
-            -0.785455934059639, 1.4112836100133928, 0.44493200318372905,
-          ]}
-          variant={"pancake_1"}
-        />
-        <Pancake
-          position={[
-            -0.785455934059639, 1.1628608589922276, 0.44493200318372905,
-          ]}
-          variant={"pancake_2"}
-        />
-        <Pancake
-          position={[
-            -0.785455934059639, 0.8879944622230104, 0.44493200318372905,
-          ]}
-          variant={"pancake_3"}
-        />
-        <Pancake
-          position={[
-            -0.785455934059639, 0.6296357686801064, 0.44493200318372905,
-          ]}
-          variant={"pancake_4"}
-        />
-        <Pancake
-          position={[
-            -0.785455934059639, 0.3594528799751239, 0.44493200318372905,
-          ]}
-          variant={"pancake_5"}
-        />
-        <Plate
-          position={[
-            -0.8403565747507878, 0.11699105745133492, 0.47439501162598885,
-          ]}
-        />
-      </group>
-    </group>
+      <ambientLight intensity={0.5} />
+      <pointLight
+        position={[
+          -0.2929949066981621, 2.6139046579002114, -2.2428363121962285,
+        ]}
+        intensity={0.2}
+        castShadow={true}
+      />
+      <pointLight
+        position={[2.1961106937285337, 1.2587410132614725, 1.637912956440818]}
+        intensity={0.5}
+        castShadow={true}
+      />
+    </>
   );
 }
--- packages/editor/src/ui/provider-config.tsx
+++ packages/editor/src/ui/provider-config.tsx
@@ -87,7 +87,7 @@ export function ProviderConfig() {
 
   return (
     <div className="pointer-events-auto w-64 self-start overflow-hidden rounded-lg border border-neutral-800 bg-neutral-900 text-white">
-      <h2 className="text-md px-4 pt-2.5 font-medium text-neutral-300">
+      <h2 className="text-md px-4 pt-3 font-medium text-neutral-300">
         Provider config
       </h2>
       <div className="-mt-0.5 mb-2 px-4">
