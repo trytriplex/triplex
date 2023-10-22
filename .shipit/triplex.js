494138445930a5aa5b13a5f4bc819df3776f923c
Michael Dougall <6801309+itsdouges@users.noreply.github.com>
2023-10-22 18:00:05 +1100

    Fix shipit config (#426)

--- .shipit/triplex.js
+++ .shipit/triplex.js
@@ -16,12 +16,8 @@ module.exports = {
       [".vscode", ".vscode"],
       ["apps/docs", "apps/docs"],
       ["examples", "examples"],
-      ["packages/bridge", "packages/bridge"],
       ["packages/create-triplex-project", "packages/create-triplex-project"],
-      ["packages/editor", "packages/editor"],
       ["packages/react-three-test", "packages/react-three-test"],
-      ["packages/run", "packages/run"],
-      ["packages/scene", "packages/scene"],
       ["oss-roots/triplex/", ""],
       ["scripts", "scripts"],
     ]);
