399713d9aeacb5996dbac7ae339dc6093ff62cb9
Michael Dougall <6801309+itsdouges@users.noreply.github.com>
2024-01-17 20:35:03 +1100

    Exclude canvas element (#579)

--- packages/@triplex/renderer-r3f/src/scene-object.tsx
+++ packages/@triplex/renderer-r3f/src/scene-object.tsx
@@ -14,6 +14,7 @@ import { useOnSceneObjectMount } from "./stores/selection";
 const EXCLUSIONS = [
   "ArcballControls",
   "CameraControls",
+  "Canvas",
   "Ecctrl",
   "FirstPersonControls",
   "FlyControls",
