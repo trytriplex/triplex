b1756033020fa4d28c1e53823dd9b0a09179b18c
Michael Dougall <6801309+itsdouges@users.noreply.github.com>
2024-03-16 08:37:12 +1100

    Add test for unsafe ref access
    adeira-source-id: 0bc979c658f6000d8eb72cbe202da60ea0e417db

---
+++ examples/test-fixture/src/unsafe-ref.tsx
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) Michael Dougall. All rights reserved.
+ *
+ * This source code is licensed under the GPL-3.0 license found in the LICENSE
+ * file in the root directory of this source tree.
+ */
+// eslint-disable-next-line react/display-name
+
+import { useLayoutEffect, useRef, useState } from "react";
+import { type Mesh } from "three";
+
+export const UnsafeRefAccess = () => {
+  const ref = useRef<Mesh>(null!);
+  const [, setRe] = useState(false);
+
+  useLayoutEffect(() => {
+    ref.current.position.x += 0.01;
+
+    return () => {
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      ref.current.position.x -= 0.01;
+    };
+  });
+
+  useLayoutEffect(() => {
+    setRe(true);
+  }, []);
+
+  return (
+    <mesh ref={ref}>
+      <boxGeometry />
+      <meshStandardMaterial />
+    </mesh>
+  );
+};
