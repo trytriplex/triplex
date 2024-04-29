a4d15adcdde3288004717170285954224f02eb72
Michael Dougall <6801309+itsdouges@users.noreply.github.com>
2024-04-29 17:15:53 +1000

    Remove hardcode VSCode config
    adeira-source-id: 6e7585da36759afe9bcd7e142f6e471266b2696a

--- examples/test-fixture/src/zcamera.tsx
+++ examples/test-fixture/src/zcamera.tsx
@@ -5,11 +5,20 @@
  * file in the root directory of this source tree.
  */
 import { PerspectiveCamera } from "@react-three/drei";
+import Box from "./geometry/box";
+
+/**
+ * Copyright (c) Michael Dougall. All rights reserved.
+ *
+ * This source code is licensed under the GPL-3.0 license found in the LICENSE
+ * file in the root directory of this source tree.
+ */
 
 export function Camera() {
   return (
     <>
       <PerspectiveCamera position={[0, 1.9, 6.7]} />
+      <Box position={[-0.08, 18.46, 64.84]} scale={0.1} />
     </>
   );
 }
