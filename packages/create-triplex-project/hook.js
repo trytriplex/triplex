ced58e9a1dc707760491f0550dd1c0b529d193a0
Michael Dougall <6801309+itsdouges@users.noreply.github.com>
2024-09-12 12:07:39 +1000

    chore: fix build

--- packages/create-triplex-project/hook.js
+++ packages/create-triplex-project/hook.js
@@ -5,9 +5,11 @@
  * This source code is licensed under the GPL-3.0 license found in the LICENSE
  * file in the root directory of this source tree.
  */
+// eslint-disable-next-line @typescript-eslint/no-require-imports
 require("ts-node").register({ project: __dirname + "/tsconfig.json" });
 
 process.env.TRIPLEX_ENV = "development";
 process.env.VITE_TRIPLEX_ENV = process.env.VITE_TRIPLEX_ENV || "development";
 
+// eslint-disable-next-line @typescript-eslint/no-require-imports
 require("./src/main.ts");
