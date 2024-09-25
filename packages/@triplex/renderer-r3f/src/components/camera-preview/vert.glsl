c86f4062ab0e49f4263a2c97822beb67a0e00f13
Michael Dougall <6801309+itsdouges@users.noreply.github.com>
2024-09-26 08:43:33 +1000

    Fix camera preview positioning (#796)

--- packages/@triplex/renderer-r3f/src/components/camera-preview/vert.glsl
+++ packages/@triplex/renderer-r3f/src/components/camera-preview/vert.glsl
@@ -1,7 +1,14 @@
 varying vec2 v_uv;
 
+float PREVIEW_SIZE = 0.33;
+float DEFAULT_Z_POS = 0.5;
+
 void main() {
   v_uv = uv;
-  vec3 offset = vec3(-1.3, -1.3, 0.0);
-  gl_Position = vec4(position + offset, 2.0);
+  vec3 offset = vec3(
+    -DEFAULT_Z_POS / PREVIEW_SIZE + DEFAULT_Z_POS,
+    -DEFAULT_Z_POS / PREVIEW_SIZE + DEFAULT_Z_POS,
+    0.0
+  );
+  gl_Position = vec4(position + offset, DEFAULT_Z_POS / PREVIEW_SIZE);
 }
