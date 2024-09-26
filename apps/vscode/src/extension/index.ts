a9898ac4ec9bfa67670e24bf6861de6170a0a770
Michael Dougall <6801309+itsdouges@users.noreply.github.com>
2024-09-26 10:10:45 +1000

    Turn off sentry in the main process (#797)

--- apps/vscode/src/extension/index.ts
+++ apps/vscode/src/extension/index.ts
@@ -5,18 +5,11 @@
  * file in the root directory of this source tree.
  */
 
-import { init } from "@sentry/node";
-import * as vscode from "vscode";
+import type * as vscode from "vscode";
 import { version } from "../../package.json";
 import * as output from "../util/log/vscode";
 import { TriplexCodelensProvider } from "./codelens";
 import { TriplexEditorProvider } from "./editor";
-
-if (process.env.NODE_ENV === "production" && vscode.env.isTelemetryEnabled) {
-  init({
-    dsn: "https://cae61a2a840cbbe7f17e240c99ad0346@o4507990276177920.ingest.us.sentry.io/4507990321725440",
-  });
-}
 
 const log = output.logger("main");
 
