3248377bbb73a4cbd28de4a45d3f6e78dd809644
Michael Dougall <6801309+itsdouges@users.noreply.github.com>
2024-10-24 15:44:09 +1100

    Extract Nextra Theme Components (#822)

---
+++ apps/docs/nextra-triplex/layout.tsx
@@ -0,0 +1,365 @@
+/**
+ * Copyright (c) Michael Dougall. All rights reserved.
+ *
+ * This source code is licensed under the GPL-3.0 license found in the LICENSE
+ * file in the root directory of this source tree.
+ */
+import {
+  ArrowLeftIcon,
+  ArrowRightIcon,
+  ChevronRightIcon,
+} from "@radix-ui/react-icons";
+import Image from "next/image";
+import Link from "next/link";
+import type { NextraThemeLayoutProps } from "nextra";
+import { normalizePages } from "nextra/normalize-pages";
+import { Fragment, useEffect, useMemo, useState } from "react";
+import { SendFeedback } from "../components/feedback";
+import { Footer } from "../components/footer";
+import { Header, HeaderItem } from "../components/header";
+import { useSearchStore } from "../stores/search";
+import { cn } from "../util/cn";
+import { friendlyDate } from "../util/date";
+import socials from "../util/socials.json";
+import { SearchModal } from "./search-modal";
+import { SideNavItem } from "./side-nav-item";
+
+function hasSelectedChild(
+  item: ReturnType<typeof normalizePages>["docsDirectories"][0],
+  route: string,
+): boolean {
+  if (!item.children) {
+    return false;
+  }
+
+  return item.children.some((child) => {
+    if (child.route === route) {
+      return true;
+    }
+
+    return hasSelectedChild(child, route);
+  });
+}
+
+function renderDocsItem(
+  link: ReturnType<typeof normalizePages>["docsDirectories"][0],
+  route: string,
+  level = 0,
+): JSX.Element | null {
+  if (!link.isUnderCurrentDocsTree || link.type === "separator") {
+    return null;
+  }
+
+  const url =
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    link.firstChildRoute || (link as any).href || link.route;
+
+  if (link.kind === "Folder") {
+    return (
+      <Fragment key={url}>
+        <div className="mt-6 py-1.5 text-base font-medium text-neutral-100">
+          {link.title}
+        </div>
+        {link.children &&
+          link.children.map((item) => renderDocsItem(item, route, level + 1))}
+      </Fragment>
+    );
+  }
+
+  return (
+    <SideNavItem
+      href={url}
+      isChildSelected={hasSelectedChild(link, route)}
+      isSelected={route === url}
+      key={url}
+      level={level}
+      title={link.title}
+    >
+      {link.children &&
+        !!link.children.length &&
+        link.children.map((item) => renderDocsItem(item, route, level + 1))}
+    </SideNavItem>
+  );
+}
+
+export function Layout({ children, pageOpts }: NextraThemeLayoutProps) {
+  const { frontMatter, headings, pageMap, route, title } = pageOpts;
+
+  const [isNavMenuOpen, setNavOpen] = useState(false);
+  const searchOpen = useSearchStore((store) => store.isOpen);
+  const closeSearch = useSearchStore((store) => () => store.setIsOpen(false));
+
+  const result = useMemo(
+    () =>
+      normalizePages({
+        list: pageMap,
+        locale: "en_US",
+        route,
+      }),
+    [pageMap, route],
+  );
+
+  const previousPage = result.flatDocsDirectories[result.activeIndex - 1];
+  const nextPage = result.flatDocsDirectories[result.activeIndex + 1];
+  const pageItems = result.flatDocsDirectories.filter(
+    (item) => item.isUnderCurrentDocsTree,
+  );
+  const selectedHeaderItem = result.topLevelNavbarItems.findLast((item) => {
+    const toFind = item.route || item.href;
+    if (toFind) {
+      return route.startsWith(toFind);
+    }
+
+    return false;
+  });
+  const hasPageAuthorOrDate = frontMatter.date || frontMatter.author;
+
+  useEffect(() => {
+    setNavOpen(false);
+  }, [route]);
+
+  return (
+    <>
+      <Header onShowNav={() => setNavOpen((prev) => !prev)}>
+        {result.topLevelNavbarItems.map((item) => (
+          <HeaderItem
+            href={item.href || item.children?.[0].route || item.route}
+            isSelected={selectedHeaderItem === item}
+            key={item.title}
+          >
+            {item.title}
+          </HeaderItem>
+        ))}
+      </Header>
+
+      {isNavMenuOpen && (
+        <div className="fixed left-0 right-0 top-14 z-40 max-h-[calc(100vh-3.5rem)] overflow-auto border-b border-neutral-900 bg-neutral-950 px-6 py-6 text-white md:hidden">
+          {result.topLevelNavbarItems.map((item) => (
+            <div className="mb-2" key={item.title}>
+              <Link
+                className="text-xl text-neutral-300 hover:text-neutral-100"
+                href={item.href || item.firstChildRoute || item.route}
+              >
+                {item.title}
+              </Link>
+            </div>
+          ))}
+
+          {pageItems.length > 0 ? (
+            <hr className="-mx-2 my-6 border-neutral-900" />
+          ) : null}
+
+          {pageItems.map((item) => (
+            <div className="mb-2" key={item.title}>
+              <Link
+                className="text-xl text-neutral-300 hover:text-neutral-100"
+                href={item.firstChildRoute || item.route}
+              >
+                {item.title}
+              </Link>
+            </div>
+          ))}
+        </div>
+      )}
+
+      {result.activeThemeContext.layout === "default" && (
+        <nav
+          aria-label="page tree"
+          className="col-span-3 hidden w-full pr-10 pt-12 md:block md:pb-10 lg:pb-14"
+        >
+          <div className="sticky top-20 flex flex-col items-start pl-6 md:pl-8 lg:pl-20">
+            {result.docsDirectories.map((item) => renderDocsItem(item, route))}
+          </div>
+        </nav>
+      )}
+
+      <main
+        className={cn([
+          "relative",
+          result.activeThemeContext.layout === "raw" && "col-span-full",
+          result.activeThemeContext.layout === "default" &&
+            "col-span-full px-6 md:col-span-9 md:pl-0 md:pr-8 lg:pr-20",
+          result.activeThemeContext.layout === "full" &&
+            "col-span-full mx-auto w-full max-w-7xl px-6",
+        ])}
+      >
+        {result.activeThemeContext.layout !== "raw" && (
+          <div
+            className="relative -mx-6 flex border-b border-t border-neutral-700 md:mx-0 md:mt-8 md:border"
+            style={{ "--grid": "100px" }}
+          >
+            <div className="relative flex basis-2/3 flex-col justify-center gap-2 px-6 py-8 [background:repeating-linear-gradient(transparent,transparent_var(--grid),#292929_var(--grid),#292929_calc(var(--grid)+1px)),repeating-linear-gradient(to_right,transparent,transparent_var(--grid),#292929_var(--grid),#292929_calc(var(--grid)+1px))] md:min-h-[16rem] md:px-14 md:py-14 lg:min-h-[20rem] lg:basis-3/5">
+              {hasPageAuthorOrDate && (
+                <div className="space-x-1">
+                  {frontMatter.date && (
+                    <time
+                      className="text-neutral-400"
+                      dateTime={frontMatter.date}
+                    >
+                      {friendlyDate(frontMatter.date)}
+                    </time>
+                  )}
+                  {frontMatter.author && frontMatter.date && (
+                    <span className="text-neutral-400"> · </span>
+                  )}
+                  {frontMatter.author && (
+                    <a
+                      className="text-neutral-400 underline hover:text-neutral-100"
+                      href={
+                        frontMatter.author in socials
+                          ? socials[frontMatter.author as keyof typeof socials]
+                              .url
+                          : "#"
+                      }
+                      rel="noreferrer"
+                      target="_blank"
+                    >
+                      {frontMatter.author}
+                    </a>
+                  )}
+                </div>
+              )}
+              {!hasPageAuthorOrDate && result.activeThemeContext.breadcrumb && (
+                <div className="flex flex-wrap items-center gap-0.5">
+                  {result.activePath.map((path, index) =>
+                    index === 0 ? null : (
+                      <Fragment key={path.route}>
+                        <Link
+                          className="text-base text-neutral-400 hover:text-neutral-300"
+                          href={path.kind === "MdxPage" ? path.route : ""}
+                        >
+                          {path.title}
+                        </Link>
+                        {index !== result.activePath.length - 1 && (
+                          <ChevronRightIcon className="text-neutral-400" />
+                        )}
+                      </Fragment>
+                    ),
+                  )}
+                </div>
+              )}
+              <h1 className="text-3xl font-bold text-neutral-200 lg:text-4xl">
+                {title}
+              </h1>
+              {frontMatter.description && (
+                <div className="text-lg text-neutral-400">
+                  {frontMatter.description}
+                </div>
+              )}
+            </div>
+            <div className="relative basis-1/2 overflow-hidden">
+              <div className="absolute -left-full bottom-0 right-0 top-0 [background:repeating-conic-gradient(#141414_0%_25%,#212121_0%_50%)_left_top/calc(var(--grid)*2+2px)_calc(var(--grid)*2+2px)]" />
+              {frontMatter.image && (
+                <Image
+                  alt=""
+                  className="rounded-none object-cover py-1 pr-1"
+                  fill
+                  src={frontMatter.image}
+                />
+              )}
+            </div>
+            <div className="pointer-events-none absolute inset-0 border-4 border-neutral-950" />
+          </div>
+        )}
+        <div
+          className={cn([
+            result.activeThemeContext.layout === "raw" && "contents",
+            result.activeThemeContext.layout !== "raw" &&
+              "grid grid-cols-12 py-10 md:px-14 lg:py-14",
+          ])}
+        >
+          <div
+            className={cn([
+              result.activeThemeContext.layout === "raw" && "contents",
+              result.activeThemeContext.layout !== "raw" &&
+                "col-span-full xl:col-span-9 [&>*:first-child]:m-0",
+            ])}
+          >
+            {children}
+
+            {(result.activeThemeContext.pagination ||
+              ("feedback" in result.activeThemeContext &&
+                typeof result.activeThemeContext.feedback === "boolean" &&
+                result.activeThemeContext.feedback)) && (
+              <div className="mt-10 flex flex-col gap-10 border-t border-neutral-800 pt-8 lg:mt-20 lg:pt-12">
+                {result.activeThemeContext.pagination && (
+                  <nav aria-label="pages navigation" className="flex">
+                    {previousPage && (
+                      <div className="flex flex-col">
+                        <span className="-mb-0.5 ml-[15px] pl-2 text-sm text-neutral-400">
+                          Previous
+                        </span>
+                        <Link
+                          className="flex items-center gap-2 text-xl text-neutral-300 hover:text-neutral-100 md:text-lg"
+                          href={previousPage.route}
+                        >
+                          <ArrowLeftIcon className="text-neutral-400" />
+                          {previousPage.title}
+                        </Link>
+                      </div>
+                    )}
+                    {nextPage && nextPage.route !== "#" && (
+                      <div className="ml-auto flex flex-col">
+                        <span className="-mb-0.5 text-sm text-neutral-400">
+                          Next
+                        </span>
+                        <Link
+                          className="flex items-center gap-2 text-xl text-neutral-300 hover:text-neutral-100 md:text-lg"
+                          href={nextPage.route}
+                        >
+                          {nextPage.title}
+                          <ArrowRightIcon className="text-neutral-400" />
+                        </Link>
+                      </div>
+                    )}
+                  </nav>
+                )}
+                {"feedback" in result.activeThemeContext &&
+                  typeof result.activeThemeContext.feedback === "boolean" &&
+                  result.activeThemeContext.feedback && <SendFeedback />}
+              </div>
+            )}
+          </div>
+          {result.activeThemeContext.layout !== "raw" && !!headings.length && (
+            <nav
+              aria-label="on this page"
+              className="col-span-3 hidden justify-self-start pl-10 xl:block"
+            >
+              <div className="sticky top-20 flex flex-col gap-2">
+                <span className="text-sm font-medium text-neutral-200">
+                  On this page
+                </span>
+                {headings.map((h) => (
+                  <div key={h.id} style={{ paddingLeft: (h.depth - 2) * 8 }}>
+                    <Link
+                      className="text-sm text-neutral-400 hover:text-neutral-200"
+                      href={`#${h.id}`}
+                    >
+                      {h.value}
+                    </Link>
+                  </div>
+                ))}
+                <div className="mt-2 border-t border-neutral-800 pt-3">
+                  <a
+                    className="text-sm text-neutral-400 hover:text-neutral-200"
+                    href={`https://github.com/try-triplex/triplex/tree/main/apps/docs/${pageOpts.filePath}`}
+                    rel="noreferrer"
+                    target="_blank"
+                  >
+                    Suggest an edit to this page
+                  </a>
+                </div>
+              </div>
+            </nav>
+          )}
+        </div>
+      </main>
+
+      <Footer />
+
+      {searchOpen && (
+        <SearchModal onClose={closeSearch} pages={result.flatDirectories} />
+      )}
+    </>
+  );
+}
---
+++ apps/docs/nextra-triplex/search-modal.tsx
@@ -0,0 +1,186 @@
+/**
+ * Copyright (c) Michael Dougall. All rights reserved.
+ *
+ * This source code is licensed under the GPL-3.0 license found in the LICENSE
+ * file in the root directory of this source tree.
+ */
+
+import Link from "next/link";
+import { type normalizePages } from "nextra/normalize-pages";
+import {
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+  type MouseEventHandler,
+} from "react";
+
+interface SearchResult {
+  id: string;
+  route: string;
+  title: string;
+}
+
+type SearchResultKeys = (keyof SearchResult)[];
+
+export function SearchModal({
+  onClose,
+  pages,
+}: {
+  onClose: () => void;
+  pages: ReturnType<typeof normalizePages>["docsDirectories"][0][];
+}) {
+  type FlexSearchType = import("flexsearch").Document<
+    SearchResult,
+    SearchResultKeys
+  >;
+
+  const ref = useRef<HTMLDialogElement>(null!);
+  const [filter, setFilter] = useState("");
+  const [index, setIndex] = useState<FlexSearchType>();
+  const searchResults: SearchResult[] = useMemo(() => {
+    if (!index) {
+      return [];
+    }
+
+    const foundFields = index.search<true>(filter, 5, {
+      enrich: true,
+      suggest: true,
+    });
+
+    if (foundFields.length === 0) {
+      return [];
+    }
+
+    const foundResults: SearchResult[] = [];
+
+    foundFields.forEach((field) => {
+      field.result.forEach((res) => {
+        foundResults.push({
+          id: res.doc.id,
+          route: res.doc.route,
+          title: res.doc.title,
+        });
+      });
+    });
+
+    return foundResults.slice(0, 5);
+  }, [filter, index]);
+
+  useEffect(() => {
+    async function prepIndex() {
+      const { default: FlexSearch } = await import("flexsearch");
+
+      if (index) {
+        return;
+      }
+
+      const newIndex = new FlexSearch.Document<SearchResult, SearchResultKeys>({
+        cache: 100,
+        context: {
+          bidirectional: true,
+          depth: 2,
+          resolution: 9,
+        },
+        document: {
+          id: "id",
+          index: ["title", "content"],
+          store: ["title", "route", "id"],
+        },
+        tokenize: "full",
+      });
+
+      pages.forEach((page) => {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        const route = (page as any).href || page.route;
+
+        const document = {
+          content: "",
+          id: route + page.title,
+          route,
+          title: page.title,
+        };
+
+        newIndex.add(document);
+      });
+
+      setIndex(newIndex);
+    }
+
+    prepIndex();
+  }, [index, pages]);
+
+  useEffect(() => {
+    const element = ref.current;
+
+    if (!element.open) {
+      element.showModal();
+    }
+
+    function onEscapePressHandler(e: Event) {
+      e.preventDefault();
+      onClose();
+    }
+
+    function onClickBlanketHandler(e: MouseEvent) {
+      if (e.target === ref.current) {
+        ref.current.close();
+        onClose();
+      }
+    }
+
+    element.addEventListener("cancel", onEscapePressHandler);
+    element.addEventListener("click", onClickBlanketHandler);
+
+    return () => {
+      element.removeEventListener("cancel", onEscapePressHandler);
+      element.removeEventListener("click", onClickBlanketHandler);
+    };
+  }, [onClose]);
+
+  const onSearchResultClickHandler: MouseEventHandler<HTMLDivElement> = (e) => {
+    if (e.target instanceof HTMLElement && e.target.tagName === "A") {
+      onClose();
+    }
+  };
+
+  return (
+    <dialog
+      className="m-auto mt-10 w-full rounded-lg border border-neutral-700 bg-neutral-950 p-0 backdrop:bg-black/70 md:mt-32 md:max-w-2xl"
+      ref={ref}
+    >
+      <form method="dialog">
+        <div className="flex items-center">
+          <input
+            className="w-full rounded bg-transparent px-4 py-4 text-lg text-neutral-200 placeholder:text-neutral-400 focus:outline-none md:text-lg"
+            onChange={(e) => setFilter(e.target.value)}
+            placeholder="Search documentation..."
+            type="text"
+          />
+
+          <div className="mr-4 rounded border border-neutral-600 bg-white/5 px-1.5 py-0.5 text-xs text-neutral-400 md:block">
+            ESC
+          </div>
+        </div>
+      </form>
+
+      {searchResults.length > 0 && (
+        <div
+          className="flex flex-col border-t border-neutral-800 py-1.5"
+          onClick={onSearchResultClickHandler}
+        >
+          {searchResults.map((res) => (
+            <Link
+              className="px-4 py-1 text-lg text-neutral-300 hover:bg-white/5 md:text-base"
+              href={res.route}
+              key={res.id}
+              target={res.route.startsWith("http") ? "_blank" : undefined}
+            >
+              {res.title}
+            </Link>
+          ))}
+        </div>
+      )}
+    </dialog>
+  );
+}
