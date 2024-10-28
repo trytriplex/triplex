6a7afb331929e91814186ba04ee5b12cb3cfc21a
Michael Dougall <6801309+itsdouges@users.noreply.github.com>
2024-10-28 16:56:47 +1100

    Building Your Scene / Components Docs (#830)

--- apps/docs/components/callouts.tsx
+++ apps/docs/components/callouts.tsx
@@ -4,7 +4,7 @@
  * This source code is licensed under the GPL-3.0 license found in the LICENSE
  * file in the root directory of this source tree.
  */
-import { BookmarkFilledIcon } from "@radix-ui/react-icons";
+import { BookmarkFilledIcon, RocketIcon } from "@radix-ui/react-icons";
 
 export function KnowledgeCallout({ children }: { children: React.ReactNode }) {
   return (
@@ -13,7 +13,25 @@ export function KnowledgeCallout({ children }: { children: React.ReactNode }) {
         <BookmarkFilledIcon />
         New Knowledge
       </span>
-      <span className="pl-8 text-xl text-neutral-300 md:text-lg [&>*]:m-0">
+      <span className="pl-8 text-xl text-neutral-300 md:text-lg [&>*:first-child]:mt-0">
+        {children}
+      </span>
+    </div>
+  );
+}
+
+export function YouWillLearnCallout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="my-10 flex flex-col gap-2.5 bg-white/10 px-6 py-8">
+      <span className="flex items-center gap-4 text-xl font-medium text-neutral-200">
+        <RocketIcon />
+        You Will Learn
+      </span>
+      <span className="pl-8 text-xl text-neutral-300 md:text-lg [&>*:first-child]:mt-0">
         {children}
       </span>
     </div>
