39ee1ba6ea5c21f546885cb85e6bd3068861a694
Michael Dougall <6801309+itsdouges@users.noreply.github.com>
2024-12-24 07:33:26 +1100

    Refactor dynamic root algorithm to apply on exit rather than immediately
    adeira-source-id: 274e8bad07b4d3b2726246528256189dd58973d5

---
+++ examples/test-fixture/src/selection-edge-case.tsx
@@ -0,0 +1,23 @@
+/**
+ * Copyright (c) Michael Dougall. All rights reserved.
+ *
+ * This source code is licensed under the GPL-3.0 license found in the LICENSE
+ * file in the root directory of this source tree.
+ */
+import { type ReactNode } from "react";
+
+function ContextProvider({ children }: { children: ReactNode }) {
+  return children;
+}
+
+export function UnknownCustomComponentResolvedHostElements() {
+  return (
+    <>
+      <ContextProvider>
+        <mesh>
+          <boxGeometry />
+        </mesh>
+      </ContextProvider>
+    </>
+  );
+}
