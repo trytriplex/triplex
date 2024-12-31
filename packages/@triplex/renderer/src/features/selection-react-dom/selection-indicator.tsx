c3ead3aff5cb8d49462dfb034c7e6d690dd42221
Michael Dougall <6801309+itsdouges@users.noreply.github.com>
2024-12-31 21:10:57 +1100

    Use resize observer for React DOM selection (#929)

--- packages/@triplex/renderer/src/features/selection-react-dom/selection-indicator.tsx
+++ packages/@triplex/renderer/src/features/selection-react-dom/selection-indicator.tsx
@@ -5,10 +5,9 @@
  * file in the root directory of this source tree.
  */
 
-import { useEvent } from "@triplex/lib";
 import { bind } from "bind-event-listener";
 import debounce from "debounce";
-import { useEffect, useReducer, useState } from "react";
+import { useEffect, useState } from "react";
 import { style } from "../../util/style";
 import { type ResolvedNode } from "./resolver";
 
@@ -29,10 +28,6 @@ const styles = style({
   },
 });
 
-function incrementReducer(value: number) {
-  return value + 1;
-}
-
 export function SelectionIndicator({
   hovered,
   selected,
@@ -40,74 +35,74 @@ export function SelectionIndicator({
   hovered: ResolvedNode | null;
   selected: ResolvedNode[];
 }) {
-  const [indicators, setIndicators] = useState<
-    { box: DOMRect | null; node: ResolvedNode }[]
-  >([]);
-  const [hoveredIndicator, setHoveredIndicator] = useState<{
-    box: DOMRect;
-    node: ResolvedNode;
-  } | null>(null);
-  const [flushCount, recalcIndicators] = useReducer(incrementReducer, 0);
+  const [selectedRects, setSelectedRects] = useState<DOMRect[]>([]);
+  const [hoveredRect, setHoveredRect] = useState<DOMRect | null>(null);
   const [hideIndicators, setHideIndicators] = useState(false);
 
-  const recalcHoverIndicator = useEvent(() => {
-    setHoveredIndicator((prevHovered) => {
-      if (!prevHovered || !(prevHovered.node.node instanceof HTMLElement)) {
-        return prevHovered;
-      }
-
-      return {
-        box: prevHovered.node.node.getBoundingClientRect(),
-        node: prevHovered.node,
-      };
-    });
-  });
-
   useEffect(() => {
-    const boxes = selected.map((node) => {
-      const box =
-        node.node instanceof HTMLElement
-          ? node.node.getBoundingClientRect()
-          : null;
-
-      return {
-        box,
-        node,
-      };
-    });
-
-    setIndicators(boxes);
-  }, [selected, flushCount]);
-
-  useEffect(() => {
-    if (!hovered || !(hovered.node instanceof HTMLElement)) {
-      setHoveredIndicator(null);
+    if (hideIndicators) {
       return;
     }
 
-    const box = hovered.node.getBoundingClientRect();
+    if (selected.length === 0) {
+      setSelectedRects([]);
+      return;
+    }
 
-    setHoveredIndicator({
-      box,
-      node: hovered,
+    const observer = new ResizeObserver((entries) => {
+      const boxes = entries.map((entry) =>
+        entry.target.getBoundingClientRect(),
+      );
+      setSelectedRects(boxes);
     });
-  }, [hovered]);
+
+    selected.forEach((node) => {
+      if (node.node instanceof Element) {
+        observer.observe(node.node);
+      }
+    });
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [hideIndicators, selected]);
 
   useEffect(() => {
-    const showIndicatorsAfterResize = debounce(() => {
-      recalcIndicators();
-      recalcHoverIndicator();
+    if (hideIndicators) {
+      return;
+    }
+
+    if (!hovered) {
+      setHoveredRect(null);
+      return;
+    }
+
+    const observer = new ResizeObserver((entry) => {
+      setHoveredRect(entry[0].target.getBoundingClientRect());
+    });
+
+    if (hovered?.node instanceof Element) {
+      observer.observe(hovered.node);
+    }
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [hideIndicators, hovered]);
+
+  useEffect(() => {
+    const debouncedShowIndicators = debounce(() => {
       setHideIndicators(false);
     }, 150);
 
     return bind(window, {
       listener: () => {
         setHideIndicators(true);
-        showIndicatorsAfterResize();
+        debouncedShowIndicators();
       },
       type: "resize",
     });
-  }, [recalcHoverIndicator]);
+  }, []);
 
   if (hideIndicators) {
     return null;
@@ -115,28 +110,29 @@ export function SelectionIndicator({
 
   return (
     <>
-      {hoveredIndicator && (
+      {hoveredRect && hovered && (
         <div
-          data-testid={`Hovered(${hoveredIndicator.node.meta.name}@${hoveredIndicator.node.meta.line}:${hoveredIndicator.node.meta.column})`}
+          data-testid={`Hovered(${hovered.meta.name}@${hovered.meta.line}:${hovered.meta.column})`}
           style={style.merge(styles.indicator, styles.hovered, {
-            height: hoveredIndicator.box.height,
-            left: hoveredIndicator.box.x,
-            top: hoveredIndicator.box.y,
-            width: hoveredIndicator.box.width,
+            height: hoveredRect.height,
+            left: hoveredRect.x,
+            top: hoveredRect.y,
+            width: hoveredRect.width,
           })}
         />
       )}
 
-      {indicators.map(({ box, node }) => {
-        if (!box) {
+      {selectedRects.map((box, index) => {
+        const node = selected.at(index);
+        if (!node) {
           return null;
         }
 
-        const { column, line, name } = node.meta;
+        const { meta } = node;
 
         return (
           <div
-            data-testid={`Selected(${name}@${line}:${column})`}
+            data-testid={`Selected(${meta.name}@${meta.line}:${meta.column})`}
             key={`${box.x}${box.y}${box.width}${box.height}`}
             style={style.merge(styles.indicator, styles.selected, {
               height: box.height,
