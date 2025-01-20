839d09f6a1c3c9440afdb9daa6b9b1286f66f058
Michael Dougall <6801309+itsdouges@users.noreply.github.com>
2025-01-20 11:03:50 +1100

    Move functional examples to private folder
    adeira-source-id: 46b65fee38918d86e12df31c59f738f124c78ff4

--- examples/js/.triplex/provider.jsx
+++
@@ -1,8 +0,0 @@
-export default function Provider({ children, color = "" }) {
-  return (
-    <>
-      {color && <color args={[color]} attach="background" />}
-      {children}
-    </>
-  );
-}
--- examples/test-fixture/src/util/errors.tsx
+++
@@ -1,9 +0,0 @@
-/**
- * Copyright (c) Michael Dougall. All rights reserved.
- *
- * This source code is licensed under the GPL-3.0 license found in the LICENSE
- * file in the root directory of this source tree.
- */
-export function throwsError(message: string = 'An error was raised.') {
-  throw new Error(message);
-}
