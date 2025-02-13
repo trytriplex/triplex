2c471dc16ec058771d5d93a4e55f6465caabf331
Michael Dougall <6801309+itsdouges@users.noreply.github.com>
2025-02-13 20:36:00 +1100

    Reset the docs landing page
    adeira-source-id: 02eb39cd4b4abfebd591e8b7d5553344dd5425b8

--- apps/docs/components/three/index.tsx
+++
@@ -1,9 +0,0 @@
-/**
- * Copyright (c) 2022—present Michael Dougall. All rights reserved.
- *
- * This repository utilizes multiple licenses across different directories. To
- * see this files license find the nearest LICENSE file up the source tree.
- */
-import dynamic from "next/dynamic";
-
-export const LazyCanvas = dynamic(() => import("./canvas"), { ssr: false });
