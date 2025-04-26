96503f51aee65508babe204280af13c112acb3d7
Michael Dougall <6801309+itsdouges@users.noreply.github.com>
2025-04-26 13:59:00 +1000

    Delete faze example
    adeira-source-id: f8bedbfa7d993fb1a9865d7d2fff4e1085dd7d39

--- examples/faze/src/types.tsx
+++
@@ -1,9 +0,0 @@
-/**
- * Copyright (c) 2022—present Michael Dougall. All rights reserved.
- *
- * This repository utilizes multiple licenses across different directories. To
- * see this files license find the nearest LICENSE file up the source tree.
- */
-export type Vector2Tuple = [x: number, z: number];
-
-export type Vector3Tuple = [x: number, y: number, z: number];
