82103a40d4f22e103614d970f948726ffb4033d4
Michael Dougall <6801309+itsdouges@users.noreply.github.com>
2025-06-02 17:54:58 +1000

    Update statsig dependencies (#1258)

---
+++ .changeset/curly-books-wink.md
@@ -0,0 +1,5 @@
+---
+"@triplex/lib": patch
+---
+
+Upgrade statsig dependencies to the latest version.
--- packages/lib/src/fg.tsx
+++ packages/lib/src/fg.tsx
@@ -5,14 +5,16 @@
  * see this files license find the nearest LICENSE file up the source tree.
  */
 /// <reference types="vite/client" />
+import { StatsigClient } from "@statsig/js-client";
+import { LocalOverrideAdapter } from "@statsig/js-local-overrides";
 import { type ReactNode } from "react";
-import Statsig from "statsig-js";
 import { type FGEnvironment } from "./types";
 
 let initialized = false;
 
 /** Overrides set for test environments. This can't be set for production builds. */
 const fgOverrides = JSON.parse(import.meta.env.VITE_FG_OVERRIDES || "{}");
+const overrideAdapter = new LocalOverrideAdapter();
 
 export async function initFeatureGates({
   environment,
@@ -35,16 +37,17 @@ export async function initFeatureGates({
 
   initialized = true;
 
-  await Statsig.initialize(
+  const client = new StatsigClient(
     "client-RoO8UZOrk5aM4zXe3AP7vQjy66PWeumvN2PfQ2P6xt7",
     { userID: userId },
     {
-      environment: {
-        tier: environment,
-      },
-      localMode: environment === "local",
+      environment: { tier: environment },
+      networkConfig: { preventAllNetworkTraffic: environment === "local" },
+      overrideAdapter,
     },
   );
+
+  await client.initializeAsync();
 
   if (typeof document !== "undefined") {
     document.documentElement.setAttribute("data-fg-user", userId);
@@ -57,15 +60,17 @@ export function fg(key: string): boolean {
     return !!fgOverrides[key];
   }
 
+  const Statsig = StatsigClient.instance();
+
   return Statsig.checkGate(key);
 }
 
 export function overrideFg(key: string, value: boolean) {
-  Statsig.overrideGate(key, value);
+  overrideAdapter.overrideGate(key, value);
 }
 
 export function clearFgOverrides() {
-  Statsig.removeGateOverride();
+  overrideAdapter.removeAllOverrides();
 }
 
 export function fgComponent<
