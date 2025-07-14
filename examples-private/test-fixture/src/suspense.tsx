6d8af237d87342617b53e7748c00d3ff2a49c6ab
Michael Dougall <6801309+itsdouges@users.noreply.github.com>
2025-07-14 21:06:14 +1000

    Fix the editor camera repositioning when the scene suspends after the initial load (#1291)

---
+++ examples-private/test-fixture/src/suspense.tsx
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) 2022—present Michael Dougall. All rights reserved.
+ *
+ * This repository utilizes multiple licenses across different directories. To
+ * see this files license find the nearest LICENSE file up the source tree.
+ */
+import { useTexture } from "@react-three/drei";
+
+export function Suspends() {
+  const map = useTexture("/images/triplex.png");
+
+  return (
+    <mesh>
+      <planeGeometry />
+      <meshBasicMaterial alphaTest={0.5} map={map} transparent />
+    </mesh>
+  );
+}
+
+export function Box() {
+  return (
+    <mesh>
+      <boxGeometry />
+    </mesh>
+  );
+}
