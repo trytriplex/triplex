4a463e89f8a5f5311088aae9c9fb440aee1258b0
Michael Dougall <6801309+itsdouges@users.noreply.github.com>
2025-07-14 21:06:14 +1000

    Fix the editor camera repositioning when the scene suspends after the initial load
    adeira-source-id: 6d8af237d87342617b53e7748c00d3ff2a49c6ab

---
+++ .changeset/tiny-buckets-explain.md
@@ -0,0 +1,5 @@
+---
+"@triplex/renderer": patch
+---
+
+The editor camera is no longer repositioned if the scene suspends after initially loading, eliminating some user jank.
--- packages/renderer/src/features/camera-helpers/camera-fit-scene.tsx
+++ packages/renderer/src/features/camera-helpers/camera-fit-scene.tsx
@@ -5,7 +5,8 @@
  * see this files license find the nearest LICENSE file up the source tree.
  */
 import { useThree } from "@react-three/fiber";
-import { useContext, useLayoutEffect } from "react";
+import { fg } from "@triplex/lib/fg";
+import { useContext, useLayoutEffect, useRef } from "react";
 import { Spherical, Vector3 } from "three";
 import { buildSceneSphere } from "../../util/three";
 import { CameraControlsContext } from "../camera-new/context";
@@ -13,10 +14,22 @@ import { CameraControlsContext } from "../camera-new/context";
 export function FitCameraToScene({ resetKeys }: { resetKeys?: string[] }) {
   const controls = useContext(CameraControlsContext);
   const scene = useThree((store) => store.scene);
+  const sceneHasBeenPositioned = useRef(false);
   const resetKey = resetKeys?.join("");
+  const lastResetKey = useRef(resetKey);
 
   useLayoutEffect(() => {
-    if (!controls) {
+    if (lastResetKey.current !== resetKey) {
+      lastResetKey.current = resetKey;
+      sceneHasBeenPositioned.current = false;
+    }
+  }, [resetKey]);
+
+  useLayoutEffect(() => {
+    if (
+      !controls ||
+      (sceneHasBeenPositioned.current && fg("fit_to_camera_fix"))
+    ) {
       return;
     }
 
@@ -38,8 +51,11 @@ export function FitCameraToScene({ resetKeys }: { resetKeys?: string[] }) {
       // Z forward rotation.
       new Vector3(0, 0, 1),
     );
+
     controls.rotateTo(point.theta, point.phi, false);
     controls.fitToSphere(sphere, false);
+
+    sceneHasBeenPositioned.current = true;
   }, [controls, scene, resetKey]);
 
   return null;
