6d8b9ce8105276ea04163f76be26a86edd8577d0
Michael Dougall <6801309+itsdouges@users.noreply.github.com>
2025-07-30 22:15:04 +1000

    Fix camera preview regression
    adeira-source-id: bf23f141afdd0901b781bda3db76e4bbafb2d313

--- packages/renderer/src/features/camera-preview/index.tsx
+++ packages/renderer/src/features/camera-preview/index.tsx
@@ -5,9 +5,9 @@
  * see this files license find the nearest LICENSE file up the source tree.
  */
 import { createPortal, useFrame, useThree } from "@react-three/fiber";
-import { useState } from "react";
-import { type Camera, type PerspectiveCamera as PCT } from "three";
-import { PerspectiveCamera, useFBO } from "triplex-drei";
+import { type Camera } from "three";
+import { useFBO } from "triplex-drei";
+import { Tunnel } from "../../components/tunnel";
 import { hash } from "../../util/hash";
 import { HIDDEN_LAYER_INDEX } from "../../util/layers";
 import frag from "./frag.glsl";
@@ -29,41 +29,40 @@ export function CameraPreview({ camera }: { camera: Camera }) {
     state.gl.setRenderTarget(null);
   });
 
-  return createPortal(
-    <mesh frustumCulled={false} layers={HIDDEN_LAYER_INDEX}>
-      <planeGeometry />
-      <shaderMaterial
-        fragmentShader={frag}
-        key={hash(frag + vert)}
-        uniforms={{
-          u_texture: { value: renderTarget.texture },
-        }}
-        vertexShader={vert}
-      />
-    </mesh>,
-    scene,
-  );
-}
-
-export function DebugCameraPreview() {
-  const [camera, setCamera] = useState<PCT | null>(null);
-
   return (
     <>
-      <pointLight intensity={50} position={[0, 1.26, 0.08]} />
-      <mesh
-        position={[0, 0, -1.92]}
-        rotation={[
-          0.805_805_810_130_845_3, 0.075_686_512_441_604_86,
-          -0.318_914_675_145_116_84,
-        ]}
-        scale={[0.52, 0.52, 0.52]}
-      >
-        <boxGeometry />
-        <meshStandardMaterial color="blue" />
-      </mesh>
-      <PerspectiveCamera ref={(val) => setCamera(val)} />
-      {camera && <CameraPreview camera={camera} />}
+      {import.meta.env.VITE_TRIPLEX_ENV === "test" && (
+        <Tunnel.In>
+          <span
+            data-testid="CameraPreview"
+            style={{
+              height: 1,
+              left: 0,
+              opacity: 0,
+              position: "absolute",
+              top: 0,
+              width: 1,
+            }}
+          >
+            {camera.name}
+          </span>
+        </Tunnel.In>
+      )}
+
+      {createPortal(
+        <mesh frustumCulled={false} layers={HIDDEN_LAYER_INDEX}>
+          <planeGeometry />
+          <shaderMaterial
+            fragmentShader={frag}
+            key={hash(frag + vert)}
+            uniforms={{
+              u_texture: { value: renderTarget.texture },
+            }}
+            vertexShader={vert}
+          />
+        </mesh>,
+        scene,
+      )}
     </>
   );
 }
